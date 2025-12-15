67f2a584057b8b14001d38d310cef4c13a620130
Khairul Ikhwan <khairul0355@hotmail.com>
2025-12-15 22:31:09 +0800

    change git to github io git

---
+++ src/js/main.js
@@ -0,0 +1,58 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, onValue, ref, set, push } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjbzbTm00LCU_c03O-ijwn-SH_I1NLoWI",
+  authDomain: "amir-dua.firebaseapp.com",
+  projectId: "amir-dua",
+  storageBucket: "amir-dua.firebasestorage.app",
+  messagingSenderId: "226472608402",
+  appId: "1:226472608402:web:315ce5eca8a1305ea81c4b",
+  databaseURL: "https://amir-dua-default-rtdb.asia-southeast1.firebasedatabase.app"
+};
+
+initializeApp(firebaseConfig);
+
+function writeUserData(name, content) {
+    
+    const db = getDatabase();
+
+    const reference = ref(db, 'fake_data/');
+
+    const pushRef = push(reference)
+
+    set(pushRef, {
+
+        username: name,
+        content: content
+    });
+}
+
+const mylist = document.getElementById('mylist');
+
+const db = getDatabase();
+
+const reference = ref(db, 'fake_data/');
+
+onValue(reference, (snapshot) => {
+
+    let list_html = '';
+
+    snapshot.forEach((childSnapshot) => {
+
+        const childKey = childSnapshot.key;
+        const childData = childSnapshot.val();
+
+        console.log('childKey');
+        console.log(childKey);
+        console.log('childData');
+        console.log(childData);
+
+        list_html = list_html + '<li>' + childData.username + ' said ' + childData.content + '</li>';
+    });
+
+    mylist.innerHTML = list_html;
+});
+// , {
+//     onlyOnce: true
+// });
