824b096bcab91bbe7a0f68bd11c28194e18bd989
Khairul Ikhwan <khairul0355@hotmail.com>
2026-01-12 01:45:56 +0800

    made timeline based on json

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,59 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap’s JS
 import * as bootstrap from 'bootstrap'
+
+import * as data from '../resource/timeline.json'
+
+let timeline_content = data.content
+console.log(data.content)
+
+for (let i = 0; i < timeline_content.length; i++) {
+
+    timeline_content[i].date = new Date(timeline_content[i].date); 
+}
+
+// result = data.sort(function(a,b) {
+//   a = a.split('/').reverse().join('');
+//   b = b.split('/').reverse().join('');
+// //   return a > b ? 1 : a < b ? -1 : 0;
+//   return a.localeCompare(b);         // <-- alternative 
+// });
+
+console.log(timeline_content.sort((a,b)=> a.date - b.date))
+
+const text_timeline_list = document.getElementById('timeline_list')
+
+let temp_timeline = ''
+
+for (let i=0; i<timeline_content.length;i++) {
+
+    let lr = 'left'
+
+    if(i % 2 == 0) {
+        lr = 'right'
+    }
+
+    temp_timeline = temp_timeline + `						<li class="timeline-item ${lr}">
+							<div class="timeline-body">
+								<div class="timeline-meta">
+									<div class="d-inline-flex">
+										<span class="text-light">Released on 05 May 2022</span>
+									</div>
+								</div>
+								<div class="timeline-content">
+									<div class="card">
+										<div class="card-body p-xl-2">
+											<h2 class="card-title mb-4">
+												${timeline_content[i].title}
+											</h2>
+											<p>
+                                            ${timeline_content[i].content}
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</li>`
+}
+
+text_timeline_list.innerHTML = temp_timeline
